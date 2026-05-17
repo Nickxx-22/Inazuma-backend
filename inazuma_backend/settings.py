dd62e4cad3698ef9f835b55a4235e325736ea538
Nickxx-22 <nicolas.bermudez.flores@alumnojoyfe.iepgroup.es>
2026-05-17 21:57:47 +0200

    fix: add sslmode require for Neon

--- inazuma_backend/settings.py
+++ inazuma_backend/settings.py
@@ -58,12 +58,15 @@ WSGI_APPLICATION = 'inazuma_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME':     os.getenv('DB_NAME', 'inazuma_db'),
-        'USER':     os.getenv('DB_USER', 'postgres'),
-        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'ENGINE':   'django.db.backends.postgresql',
+        'NAME':     os.getenv('DB_NAME'),
+        'USER':     os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST':     os.getenv('DB_HOST', 'localhost'),
         'PORT':     os.getenv('DB_PORT', '5432'),
+        'OPTIONS':  {
+            'sslmode': 'require',
+        },
     }
 }
 
